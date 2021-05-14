953320c7fd1f7164995d1c4ec88ae080d23aac63
Morten Möller <git@mortenm.de>
2021-05-14 12:13:29 +0200

    Add Navigation and improve styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
 import Header from './components/Header/Header'
+import Navigation from './components/Navigation/Navigation'
 
 function App() {
   const urlChar = 'https://rickandmortyapi.com/api/character'
@@ -18,6 +19,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Navigation />
       {chars.map(el => Card(el))}
     </div>
   )
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import './Navigation.css'
+
+export default function Navigation() {
+  return (
+    <nav className="Navigation">
+      <button className="Navigation__button">Characters</button>
+      <button className="Navigation__button">Episodes</button>
+      <button className="Navigation__button">Locations</button>
+    </nav>
+  )
+}
