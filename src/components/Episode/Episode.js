ed83215e14178e6e0df92d1f22c21d475ed2a067
Morten Möller <git@mortenm.de>
2021-05-16 22:48:21 +0200

    Edit Episode styling

--- src/components/Episode/Episode.js
+++ src/components/Episode/Episode.js
@@ -1,9 +1,18 @@
 import './Episode.css'
 
-export default function Episode({ id, name }) {
+export default function Episode({ id, name, episode, air_date }) {
+  const splitDate = air_date.split(' ')
+  const day = [splitDate[1].replace(',', '.')]
+  splitDate.splice(1, 1)
+  const dateFormated = day.concat(splitDate).join(' ')
   return (
     <section key={id} className="Episode">
       <h2 className="Episode__heading">{name}</h2>
+      <small>{episode}</small>
+      <ul className="Episode__list">
+        <li>First time on air:</li>
+        <li>{dateFormated}</li>
+      </ul>
     </section>
   )
 }
