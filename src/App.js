1a5304d91f9dcff79685eb030b99fc581ae884f9
Morten Möller <git@mortenm.de>
2021-05-17 22:45:13 +0200

    Add fetch for all pages

--- src/App.js
+++ src/App.js
@@ -3,22 +3,45 @@ import './App.css'
 import Header from './components/Header/Header'
 
 function App() {
-  const urlChar = 'https://rickandmortyapi.com/api/character'
-  const [chars, setChars] = useState([])
+  const initialUrlCharacter = 'https://rickandmortyapi.com/api/character'
+  const [characterPages, setCharacterPages] = useState([])
 
-  useEffect(
-    () =>
-      fetch(urlChar)
-        .then(res => res.json())
-        .then(data => setChars(() => setChars([...chars, data]))),
-    [urlChar]
-  )
+  const initialUrlLocation = 'https://rickandmortyapi.com/api/location'
+  const [locationPages, setLocationPages] = useState([])
+
+  const initialUrlEpisode = 'https://rickandmortyapi.com/api/episode'
+  const [episodePages, setEpisodePages] = useState([])
+
+  useEffect(() => {
+    fetchPages(initialUrlCharacter, setCharacterPages, characterPages)
+    fetchPages(initialUrlLocation, setLocationPages, locationPages)
+    fetchPages(initialUrlEpisode, setEpisodePages, episodePages)
+  }, [])
 
   return (
     <div className="App">
       <Header />
+      {console.log(characterPages)}
+      {console.log(locationPages)}
+      {console.log(episodePages)}
     </div>
   )
+}
+
+function fetchPages(initialUrl, setState, state) {
+  fetch(initialUrl)
+    .then(res => res.json())
+    .then(firstPage => {
+      const promises = Array(firstPage.info.pages)
+        .fill()
+        .map((_, index) =>
+          fetch(initialUrl + '?page=' + (index + 1)).then(res => res.json())
+        )
+      Promise.all(promises).then(otherPages =>
+        setState([...state, ...otherPages])
+      )
+    })
+    .catch(error => console.log(error))
 }
 
 export default App
